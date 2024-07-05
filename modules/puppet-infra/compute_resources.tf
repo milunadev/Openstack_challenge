cc679895a078015a9e860bffb3eb8b76565abbcb
MiLunaDEV <misusuyatec@gmail.com>
2024-07-04 21:41:47 -0500

    ADD: SSH Key resoruces

--- modules/puppet-infra/compute_resources.tf
+++ modules/puppet-infra/compute_resources.tf
@@ -1,5 +1,11 @@
+resource "tls_private_key" "puppet_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "openstack_compute_keypair_v2" "puppet_keypair" {
   name = "puppet-keypair"
+  public_key = tls_private_key.puppet_key.public_key_openssh
 }
 
 data "openstack_images_image_v2" "puppet_instance_image" {
