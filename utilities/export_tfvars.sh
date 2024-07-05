218e36a7e47c93b355b6ea7a65e1253ffa9b9faf
MiLunaDEV <misusuyatec@gmail.com>
2024-07-05 16:41:18 -0500

    ADD UTILITIES SCRIPT

--- utilities/export_tfvars.sh
+++ utilities/export_tfvars.sh
@@ -6,6 +6,9 @@ echo "------------------------------"
 
 CONFIG_FILE="$HOME/.bashrc"
 
+# Cargar las variables actuales
+source "$CONFIG_FILE"
+
 function set_env_variable {
   local var_name=$1
   local var_value=$2
@@ -16,22 +19,31 @@ function set_env_variable {
   fi
 }
 
-read -p "Auth URL (format: 'http://x.x.x.x:5000'): " os_auth_url
+# Función para leer valores con predeterminados
+function read_with_default {
+  local prompt=$1
+  local default=$2
+  local var
+  read -p "$prompt [$default]: " var
+  echo "${var:-$default}"
+}
+
+os_auth_url=$(read_with_default "Auth URL (format: 'http://x.x.x.x:5000')" "$TF_VAR_os_auth_url")
 while [[ -z "$os_auth_url" ]]; do
   echo "Auth URL is required!"
-  read -p "Auth URL (format: 'http://x.x.x.x:5000'): " os_auth_url
+  os_auth_url=$(read_with_default "Auth URL (format: 'http://x.x.x.x:5000')" "$TF_VAR_os_auth_url")
 done
 
-read -p "Project Name [$os_project_name]: " os_project_name
+os_project_name=$(read_with_default "Project Name" "$TF_VAR_os_project_name")
 while [[ -z "$os_project_name" ]]; do
   echo "Project Name is required!"
-  read -p "Project Name [$os_project_name]: " os_project_name
-done
+  os_project_name=$(read_with_default "Project Name" "$TF_VAR_os_project_name")
+end
 
-read -p "Username [$os_username]: " os_username
+os_username=$(read_with_default "Username" "$TF_VAR_os_username")
 while [[ -z "$os_username" ]]; do
   echo "Username is required!"
-  read -p "Username [$os_username]: " os_username
+  os_username=$(read_with_default "Username" "$TF_VAR_os_username")
 done
 
 read -s -p "Password: " os_password
@@ -42,18 +54,17 @@ while [[ -z "$os_password" ]]; do
   echo
 done
 
-read -p "Region Name [$os_region_name]: " os_region_name
+os_region_name=$(read_with_default "Region Name" "$TF_VAR_os_region_name")
 while [[ -z "$os_region_name" ]]; do
   echo "Region Name is required!"
-  read -p "Region Name [$os_region_name]: " os_region_name
+  os_region_name=$(read_with_default "Region Name" "$TF_VAR_os_region_name")
 done
 
-read -p "User Domain Name ('Default') [Default]: " os_user_domain_name
+os_user_domain_name=$(read_with_default "User Domain Name ('Default')" "${TF_VAR_os_user_domain_name:-Default}")
 os_user_domain_name=${os_user_domain_name:-Default}
 
-read -p "Project Domain ID ('default') [default]: " os_project_domain_id
+os_project_domain_id=$(read_with_default "Project Domain ID ('default')" "${TF_VAR_os_project_domain_id:-default}")
 os_project_domain_id=${os_project_domain_id:-default}
-
 
 export TF_VAR_os_auth_url=$os_auth_url
 export TF_VAR_os_project_name=$os_project_name
@@ -65,7 +76,7 @@ export TF_VAR_os_project_domain_id=$os_project_domain_id
 
 # Set variables permanently in the shell config file
 set_env_variable "TF_VAR_os_auth_url" "$os_auth_url"
-set_env_variable "TF_VAR_os_project" "$os_project_name"
+set_env_variable "TF_VAR_os_project_name" "$os_project_name"
 set_env_variable "TF_VAR_os_username" "$os_username"
 set_env_variable "TF_VAR_os_password" "$os_password"
 set_env_variable "TF_VAR_os_region_name" "$os_region_name"
