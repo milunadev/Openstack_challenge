73f71c70bf172dbd162bf300dc45cb87c15ae587
MiLunaDEV <misusuyatec@gmail.com>
2024-07-05 19:57:08 -0500

    update tftest files

--- testing_dir/main.py
+++ testing_dir/main.py
@@ -12,15 +12,13 @@ def load_tf_vars():
         'os_user_domain_name': os.getenv('TF_VAR_os_user_domain_name'),
         'os_project_domain_id': os.getenv('TF_VAR_os_project_domain_id')
     }
-    print(tf_vars)  # Agrega esto para verificar las variables
+    print(f"Loaded TF Vars: {tf_vars}")  # Imprimir variables para depuración
     return tf_vars
 
-
-vars = load_tf_vars()
-
+vars = load_tf_vars()  # Llamar explícitamente a la función y almacenar las variables
 
 @pytest.fixture
-def plan(): 
+def plan():
     tf = tftest.TerraformTest(tfdir='..')
     tf.setup()
     plan = tf.plan(output=True, tf_vars=vars)
@@ -28,15 +26,17 @@ def plan():
 
 def test_variables(plan):
     tf_vars = plan.variables
+    print(f"Terraform Variables: {tf_vars}")
     assert "os_username" in tf_vars, "Missing os_username"
     assert "os_project_name" in tf_vars, "Missing os_project_name"
 
-# def test_outputs(plan):
-#     outputs = plan.outputs
-#     assert "puppet_agents_ips" in outputs
-#     assert "puppet_master_ip" in outputs
-#     assert "puppet_db_ip" in outputs
+def test_outputs(plan):
+    outputs = plan.outputs
+    print(f"Terraform Outputs: {outputs}")
+    assert "puppet_agents_ips" in outputs
+    assert "puppet_master_ip" in outputs
+    assert "puppet_db_ip" in outputs
 
 if __name__ == "__main__":
-    
+    print("Running tests...")
     pytest.main()
