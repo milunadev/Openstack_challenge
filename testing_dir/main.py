260bc42d4fca49461a59d7a2a15ac0141bb08230
MiLunaDEV <misusuyatec@gmail.com>
2024-07-05 19:50:53 -0500

    update tftest files

--- testing_dir/main.py
+++ testing_dir/main.py
@@ -3,7 +3,7 @@ import tftest
 import pytest
 
 def load_tf_vars():
-    return {
+    tf_vars = {
         'os_username': os.getenv('TF_VAR_os_username'),
         'os_project_name': os.getenv('TF_VAR_os_project_name'),
         'os_password': os.getenv('TF_VAR_os_password'),
@@ -12,12 +12,16 @@ def load_tf_vars():
         'os_user_domain_name': os.getenv('TF_VAR_os_user_domain_name'),
         'os_project_domain_id': os.getenv('TF_VAR_os_project_domain_id')
     }
+    print(tf_vars)  # Agrega esto para verificar las variables
+    return tf_vars
+
 
 vars = load_tf_vars()
 
+
 @pytest.fixture
 def plan():
-    tf = tftest.TerraformTest(tfdir='../')
+    tf = tftest.TerraformTest(tfdir='..')
     tf.setup()
     plan = tf.plan(output=True, tf_vars=vars)
     return plan
