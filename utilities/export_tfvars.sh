1f237adb38e340c76fc660114b2de7aef4c02d57
MiLunaDEV <misusuyatec@gmail.com>
2024-07-08 16:14:13 -0500

    Update utilities

--- utilities/export_tfvars.sh
+++ utilities/export_tfvars.sh
@@ -13,21 +13,11 @@ read_and_export_variable() {
     echo "export $var_name='$var_value'" >> "$CONFIG_FILE"
 }
 
-set_env_variable() {
-    local var_name=$1
-    local var_value=$2
-    if grep -q "export $var_name=" "$CONFIG_FILE"; then
-        sed -i "s|export $var_name=.*|export $var_name='$var_value'|" "$CONFIG_FILE"
-    else
-        echo "export $var_name='$var_value'" >> "$CONFIG_FILE"
-    fi
-}
 
 variables=("os_auth_url" "os_project_name" "os_username" "os_password" "os_region_name" "os_user_domain_name" "os_project_domain_id")
 
 for variable in "${variables[@]}"; do
     read_and_export_variable "TF_VAR_$variable"
-    set_env_variable "TF_VAR_$variable" "$(eval echo \$TF_VAR_$variable)"
 done
 
 source "$CONFIG_FILE"
