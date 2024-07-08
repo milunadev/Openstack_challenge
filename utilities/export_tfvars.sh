28080fd36e4756ae97820d906dc3d9cc8099ed34
MiLunaDEV <misusuyatec@gmail.com>
2024-07-08 15:57:04 -0500

    Update db_role files

--- utilities/export_tfvars.sh
+++ utilities/export_tfvars.sh
@@ -13,18 +13,27 @@ read_and_export_variable() {
     echo "export $var_name=\"$var_value\"" >> "$CONFIG_FILE"
 }
 
+set_env_variable() {
+    local var_name=$1
+    local var_value=$2
+    if grep -q "export $var_name=" "$CONFIG_FILE"; then
+        sed -i "s|export $var_name=.*|export $var_name=\"$var_value\"|" "$CONFIG_FILE"
+    else
+        echo "export $var_name=\"$var_value\"" >> "$CONFIG_FILE"
+    fi
+}
+
 variables=("os_auth_url" "os_project_name" "os_username" "os_password" "os_region_name" "os_user_domain_name" "os_project_domain_id")
 
 for variable in "${variables[@]}"; do
     read_and_export_variable "TF_VAR_$variable"
+    set_env_variable "TF_VAR_$variable" "${!variable}"
 done
 
 source "$CONFIG_FILE"
 
 echo "Environment variables set:"
-echo "TF_VAR_os_auth_url=\"$TF_VAR_os_auth_url\""
-echo "TF_VAR_os_project_name=\"$TF_VAR_os_project_name\""
-echo "TF_VAR_os_username=\"$TF_VAR_os_username\""
-echo "TF_VAR_os_region_name=\"$TF_VAR_os_region_name\""
-echo "TF_VAR_os_user_domain_name=\"$TF_VAR_os_user_domain_name\""
-echo "TF_VAR_os_project_domain_id=\"$TF_VAR_os_project_domain_id\""
+for variable in "${variables[@]}"; do
+    var_name="TF_VAR_$variable"
+    echo "$var_name=\"${!var_name}\""
+done
