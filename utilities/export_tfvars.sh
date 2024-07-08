1f6257c7321b426c1ac32957ec20785a3da324ff
MiLunaDEV <misusuyatec@gmail.com>
2024-07-08 16:16:36 -0500

    Update utilities

--- utilities/export_tfvars.sh
+++ utilities/export_tfvars.sh
@@ -13,17 +13,18 @@ read_and_export_variable() {
     echo "export $var_name='$var_value'" >> "$CONFIG_FILE"
 }
 
-
 variables=("os_auth_url" "os_project_name" "os_username" "os_password" "os_region_name" "os_user_domain_name" "os_project_domain_id")
 
 for variable in "${variables[@]}"; do
     read_and_export_variable "TF_VAR_$variable"
 done
 
+# Load the updated .bashrc to make the variables available in the current session
 source "$CONFIG_FILE"
 
 echo "Environment variables set:"
 for variable in "${variables[@]}"; do
     var_name="TF_VAR_$variable"
-    echo "$var_name=\"$(eval echo \$$var_name)\""
+    eval var_value=\$$var_name
+    echo "$var_name=\"$var_value\""
 done
