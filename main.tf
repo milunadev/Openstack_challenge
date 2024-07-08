703516d1a67aedda07d079c42ba38fa0022f84dc
MiLunaDEV <misusuyatec@gmail.com>
2024-07-07 20:19:43 -0500

    UPDATE ansible files to sign files

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "null_resource" "wait_for_public_instance" {
   depends_on = [module.puppet-infra]
 }
 
-resource "null_resource" "provision_ansible" {
+resource "null_resource" "provision_puppet_agent" {
   depends_on = [module.puppet-infra, local_file.inventory, null_resource.wait_for_public_instance]
 
   provisioner "local-exec" {
@@ -50,11 +50,26 @@ resource "null_resource" "provision_ansible" {
       ssh-keyscan -H ${module.puppet-infra.public_instance_ip} >> ~/.ssh/known_hosts
       
       ssh -i ../puppetkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${module.puppet-infra.public_instance_ip} << 'EOF'
-        ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/site.yml --extra-vars "puppet_server_ip=${module.puppet-infra.puppet_server_ip} puppet_db_ip=${module.puppet-infra.puppet_db_ip} puppet_server_hostname=${module.puppet-infra.puppet_server_name} puppet_db_hostname=${module.puppet-infra.puppet_db_name}"
+        ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/site.yml --extra-vars "puppet_server_ip=${module.puppet-infra.puppet_server_ip} puppet_db_ip=${module.puppet-infra.puppet_db_ip} puppet_server_hostname=${module.puppet-infra.puppet_server_name} puppet_db_hostname=${module.puppet-infra.puppet_db_name}" --tags puppet_agent
       EOF
     EOT
   }
 
 }
 
-#
+resource "null_resource" "provision_puppet_server" {
+  depends_on = [module.puppet-infra, local_file.inventory, null_resource.wait_for_public_instance]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      scp -i ../puppetkey.pem -o StrictHostKeyChecking=no -r ./ansible_dir/* ubuntu@${module.puppet-infra.public_instance_ip}:/home/ubuntu/ansible_dir
+      
+      ssh-keyscan -H ${module.puppet-infra.public_instance_ip} >> ~/.ssh/known_hosts
+      
+      ssh -i ../puppetkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${module.puppet-infra.public_instance_ip} << 'EOF'
+        ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/site.yml  --extra-vars "puppet_server_ip=${module.puppet-infra.puppet_server_ip} puppet_db_ip=${module.puppet-infra.puppet_db_ip} puppet_server_hostname=${module.puppet-infra.puppet_server_name} puppet_db_hostname=${module.puppet-infra.puppet_db_name}" --tags puppet_server
+      EOF
+    EOT
+  }
+
+}
