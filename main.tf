ec9769f06bf7e6b62dc618c941df84d0f7ad190f
MiLunaDEV <misusuyatec@gmail.com>
2024-07-07 22:44:03 -0500

    UPDATE ansible files to sign files

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "local_file" "inventory" {
 
 resource "null_resource" "wait_for_public_instance" {
   provisioner "local-exec" {
-    command = "sleep 10 && ping -c 8 ${module.puppet-infra.public_instance_ip}"
+    command = "sleep 40 && ping -c 8 ${module.puppet-infra.public_instance_ip}"
   }
 
   depends_on = [module.puppet-infra]
@@ -68,6 +68,31 @@ resource "null_resource" "provision_puppet_server" {
     command = <<-EOT
       ssh -i ../puppetkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${module.puppet-infra.public_instance_ip} << 'EOF'
         ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/site.yml  --extra-vars "puppet_server_ip=${module.puppet-infra.puppet_server_ip} puppet_db_ip=${module.puppet-infra.puppet_db_ip} puppet_server_hostname=${module.puppet-infra.puppet_server_name} puppet_db_hostname=${module.puppet-infra.puppet_db_name}" --tags puppet_server
+      EOF
+    EOT
+  }
+}
+
+
+resource "null_resource" "request_certificate" {
+  depends_on = [ null_resource.provision_puppet_server ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      ssh -i ../puppetkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${module.puppet-infra.public_instance_ip} << 'EOF'
+        ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/playbooks/request_certificate.yml
+      EOF
+    EOT
+  }
+}
+
+resource "null_resource" "sign_certificate" {
+  depends_on = [ null_resource.request_certificate ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      ssh -i ../puppetkey.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${module.puppet-infra.public_instance_ip} << 'EOF'
+        ansible-playbook -i /home/ubuntu/ansible_dir/inventory/hosts.ini /home/ubuntu/ansible_dir/playbooks/sign_certificate.yml
       EOF
     EOT
   }
